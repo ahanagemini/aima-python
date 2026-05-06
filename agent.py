db429ca0928668de5e738c0ebe9503ca86219f0c
ahanagemini <ahanagemini@gmail.com>
2026-05-06 17:25:46 -0400

    Creating assignment

---
+++ agent.py
@@ -0,0 +1,14 @@
+def ReflexVacuumAgent(percept):
+    """
+    Implementation of the algorithm in Figure 2.8.
+    Input: percept - A tuple (location, status)
+    Output: action - 'Suck', 'Left', or 'Right'
+    """
+    location, status = percept
+
+    # TODO: Implement Figure 2.8 logic
+    # 1. If status is Dirty, return Suck
+    # 2. If location is A, return Right
+    # 3. If location is B, return Left
+
+    return None
